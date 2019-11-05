c118acfa0d5462ad06e6ff88e810e0253df3962d
Rafal Kruk <kruku@o2.pl>
2019-11-05 21:00:46 +0100

    Excercises 2.1 - 2.2

---
+++ src/main/java/com/github/rkruk/spring/demo/controllers/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.github.rkruk.spring.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+
+public class RegistrationController {
+
+    @GetMapping
+    public String prepareRegistrationPage() {
+        return "/WEB-INF/views/registration-form.jsp";
+    }
+
+    @PostMapping
+    public String processRegistrationPage() {
+        return "";
+    }
+}
---
+++ src/main/java/com/github/rkruk/spring/demo/model/domain/User.java
@@ -0,0 +1,100 @@
+package com.github.rkruk.spring.demo.model.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="users")
+
+@Getter
+@Setter
+
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) &&
+                username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
